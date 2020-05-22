dc29f2854e73afb25a53182bc0677892b9067f86
Flor21 <fas-2196@hotmail.com>
2020-05-22 19:11:19 -0300

    Form added in contact component

--- app/test-app/src/components/main/Contact.js
+++ app/test-app/src/components/main/Contact.js
@@ -1,20 +1,86 @@
 import React from 'react';
 import Header from '../Header';
 import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import {Row, Col} from 'react-bootstrap';
+import ModalNew from './ModalNew';
 
 export default class Contact extends React.Component {
-
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            showComponent:false,
+        };
+    }
+    active() {
+        this.setState({
+            showComponent: true,
+        });
+    }
     render(){
         return(
             <div className="contact">
                 <Header /> 
                 <Card>
-                    <Card.Header>Contact information</Card.Header>
+                    <Card.Header> <h6> Contact information </h6></Card.Header>
                     <Card.Body>
-                        <Card.Title>Tel: 555-891</Card.Title>
-                        <Card.Text>
-                            Place: P. Sherman, calle Wallaby, 42, Sydney
-                        </Card.Text>
+                        <Form>
+                        {this.state.showComponent ? 
+                                <ModalNew /> : 
+                            <>
+                            <Form.Group as={Row} controlId="formHorizontalSurname">
+                                <Form.Label column sm={2}>
+                                    Surname:
+                                </Form.Label>
+                                <Col sm={10}>
+                                <Form.Control type="surname" required />
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group as={Row} controlId="formHorizontalName">
+                                <Form.Label column sm={2}>
+                                    Name:
+                                </Form.Label>
+                                <Col sm={10}>
+                                <Form.Control type="name" required />
+                                </Col>
+                            </Form.Group>
+                            
+                            <Form.Group as={Row} controlId="formHorizontalPhone">
+                                <Form.Label column sm={2}>
+                                    Phone:
+                                </Form.Label>
+                                <Col sm={10}>
+                                <Form.Control type="phone"required />
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group as={Row} controlId="formHorizontalEmail">
+                                <Form.Label column sm={2}>
+                                    Email:
+                                </Form.Label>
+                                <Col sm={10}>
+                                <Form.Control type="email" required />
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group as={Row} controlId="formHorizontalEmail">
+                                <Form.Label column sm={2}>
+                                    Message:
+                                </Form.Label>
+                                <Col sm={10}>
+                                <textarea name="msj" rows="5" cols="44" required ></textarea>  
+                                </Col>
+                            </Form.Group>
+                            
+                            <Button className="btt" variant="outline-lightblue" onClick={this.active.bind(this)}>
+                                Send
+                            </Button>
+                            </>
+                            }
+                        </Form>
                     </Card.Body>
                 </Card>
             </div>
