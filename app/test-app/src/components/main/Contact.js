63eb20b3a0508571df29464b0bab3a63e680f87f
Flor21 <fas-2196@hotmail.com>
2020-05-24 13:51:47 -0300

    Fill in all the fields to send the form in the contact component

--- app/test-app/src/components/main/Contact.js
+++ app/test-app/src/components/main/Contact.js
@@ -5,20 +5,40 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import {Row, Col} from 'react-bootstrap';
 import ModalNew from './ModalNew';
+import Alert from 'react-bootstrap/Alert';
 
 export default class Contact extends React.Component {
-    
-    constructor(props) {
+
+    constructor(props){
         super(props);
-        this.state = {
+        this.state = ({
+            surname:'',
+            namE: '',
+            phone:'',
+            email:'',
+            msj:'',
             showComponent:false,
-        };
-    }
-    active() {
-        this.setState({
-            showComponent: true,
         });
     }
+    
+
+    change = e => {
+         this.setState({
+            [e.target.name]: e.target.value
+        })      
+    }
+
+    active() {
+        console.log('active',this.state)
+        if( this.state.surname !== '' && this.state.name !== '' && this.state.phone !== '' && this.state.email !== '' && this.state.msj !== '') {
+            this.setState({ showComponent: true, })   
+        }
+    }
+
+    submit = e => {
+        e.preventDefault();
+    }
+
     render(){
         return(
             <div className="contact">
@@ -26,16 +46,19 @@ export default class Contact extends React.Component {
                 <Card>
                     <Card.Header> <h6> Contact information </h6></Card.Header>
                     <Card.Body>
-                        <Form>
+                        <Form onSubmit={this.submit}>
                         {this.state.showComponent ? 
                                 <ModalNew /> : 
                             <>
+                            <Alert  variant="danger">
+                                You must complete all the fields.
+                            </Alert>
                             <Form.Group as={Row} controlId="formHorizontalSurname">
                                 <Form.Label column sm={2}>
                                     Surname:
                                 </Form.Label>
                                 <Col sm={10}>
-                                <Form.Control type="surname" required />
+                                <Form.Control value={this.state.surname} name="surname" onChange={this.change} />
                                 </Col>
                             </Form.Group>
 
@@ -44,7 +67,7 @@ export default class Contact extends React.Component {
                                     Name:
                                 </Form.Label>
                                 <Col sm={10}>
-                                <Form.Control type="name" required />
+                                <Form.Control  name="namE" value={this.state.namE} onChange={this.change} />
                                 </Col>
                             </Form.Group>
                             
@@ -53,7 +76,7 @@ export default class Contact extends React.Component {
                                     Phone:
                                 </Form.Label>
                                 <Col sm={10}>
-                                <Form.Control type="phone"required />
+                                <Form.Control name="phone" value={this.state.phone} onChange={this.change} />
                                 </Col>
                             </Form.Group>
 
@@ -62,16 +85,16 @@ export default class Contact extends React.Component {
                                     Email:
                                 </Form.Label>
                                 <Col sm={10}>
-                                <Form.Control type="email" required />
+                                <Form.Control name="email" value={this.state.email} onChange={this.change} />
                                 </Col>
                             </Form.Group>
 
-                            <Form.Group as={Row} controlId="formHorizontalEmail">
+                            <Form.Group as={Row} controlId="formHorizontalMsj">
                                 <Form.Label column sm={2}>
                                     Message:
                                 </Form.Label>
                                 <Col sm={10}>
-                                <textarea name="msj" rows="5" cols="44" required ></textarea>  
+                                <textarea name="msj" rows="5" cols="44" value={this.state.msj} onChange={this.change}></textarea>  
                                 </Col>
                             </Form.Group>
                             
